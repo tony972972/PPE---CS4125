29aa2b32525390c01051927cba797f73ed835206
tony972972 <37070065+tony972972@users.noreply.github.com>
2020-12-10 13:06:21 +0000

    added a new menu bar with sub menus

--- src/welcome/Welcome.java
+++ src/welcome/Welcome.java
@@ -46,6 +46,7 @@ public class Welcome extends JFrame implements ActionListener {
     private JPanel topJPanel;
     private JPanel centerJPanel;
     private JPanel bottomJPanel;
+    private JMenuBar menuBar = new JMenuBar(); // Window menu bar
 
     public Welcome(String username) {
        
@@ -90,9 +91,31 @@ public class Welcome extends JFrame implements ActionListener {
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Welcome " + username);
         
-        setSize(800, 750);
+        setSize(800, 800);
         setResizable(false);
         this.setLocationRelativeTo(null);
+        
+          JMenu menu, submenu;  
+          JMenuItem i1, i2, i3, i4, i5;  
+          
+           // features of the menu
+          JMenuBar mb=new JMenuBar();  
+          menu=new JMenu("Menu");  
+          submenu=new JMenu("Sub Menu");  
+          i1=new JMenuItem("Item 1");  
+          i2=new JMenuItem("Item 2");  
+          i3=new JMenuItem("Item 3");  
+          i4=new JMenuItem("Item 4");  
+          i5=new JMenuItem("Item 5");  
+          menu.add(i1); menu.add(i2); menu.add(i3);  
+          submenu.add(i4); submenu.add(i5);  
+          menu.add(submenu);  
+          mb.add(menu);  
+          setJMenuBar(mb); 
+          
+            
+    
+    
     }
 
     @Override
